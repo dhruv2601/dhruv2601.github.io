86950aa7bb423f2eb11893ba66de91e277e86164
dhruv2601 <dhruvrathi15@gmail.com>
2021-01-02 18:47:40 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ea110a1f2cdf32b03c81.js"
+    "url": "webpack-runtime-272a54b14e030c568cf2.js"
   },
   {
     "url": "styles.1ccb6da314e392a77459.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f99a96e64a57827093350d43f9f7848e"
+    "revision": "6a7b9963d091f57e502a70d918bea861"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-d3ee03be2ef85643f368.js"
